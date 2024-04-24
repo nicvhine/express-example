c7099e4bdb62c64f1f7389eecf3261dbd8d9e014
pawelsz <pawel82@gmail.com>
2024-04-24 09:51:32 +0800

    init

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var express = require('express');
-var cors = require('cors');
 var app = express();
 
 app.set('view engine', 'ejs')
-app.use(cors())
-app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('index')
 })
 
 app.post('/', (req, res) => {
